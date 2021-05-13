8e0cde834535ed94e382d9f0b225d3a4c835cc28
Eric Armijo <rcrmj@hotmail.com>
2021-05-12 21:40:11 -0400

    Script base nube palabras

---
+++ demo_word_cloud.R
@@ -0,0 +1,9 @@
+# How to Generate Word Clouds in R
+# Ref: https://towardsdatascience.com/create-a-word-cloud-with-r-bde3e7422e8a
+
+#install.packages("wordcloud")
+library(wordcloud)
+#install.packages("RColorBrewer")
+library(RColorBrewer)
+#install.packages("wordcloud2")
+library(wordcloud2)
