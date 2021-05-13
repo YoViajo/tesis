9a321fc19bdd2c02588a4e7acba7cb42a241b196
Eric Armijo <rcrmj@hotmail.com>
2021-05-12 22:19:50 -0400

    Segunda demo de nube de palabras

---
+++ demo_word_cloud_2.R
@@ -0,0 +1,58 @@
+# Generating Word Cloud in R Programming
+# Ref: https://www.geeksforgeeks.org/generating-word-cloud-in-r-programming/
+
+# Step 2: Install and Load the Required Packages
+# install the required packages
+install.packages("tm")           # for text mining
+install.packages("SnowballC")    # for text stemming
+install.packages("wordcloud")    # word-cloud generator
+install.packages("RColorBrewer") # color palettes
+
+# load the packages
+library("tm")
+library("SnowballC")
+library("wordcloud")
+library("RColorBrewer")
+
+# Step 3: Text Mining
+
+# Start by importing text file created in step 1:
+text = readLines(file.choose())
+
+# Load the data as a corpus:
+# VectorSource() function 
+# creates a corpus of 
+# character vectors
+docs = Corpus(VectorSource(text))
+
+# Text transformation:
+toSpace = content_transformer (function (x, pattern) gsub(pattern, " ", x))
+docs1 = tm_map(docs, toSpace, "/")
+docs1 = tm_map(docs, toSpace, "@")
+docs1 = tm_map(docs, toSpace, "#")
+
+# Cleaning the Text:
+# Convert the text to lower case
+docs1 = tm_map(docs1, content_transformer(tolower))
+
+# Remove numbers
+docs1 = tm_map(docs1, removeNumbers)
+
+# Remove white spaces
+docs1 = tm_map(docs1, stripWhitespace)
+
+# Step 4: Build a term-document Matrix
+dtm = TermDocumentMatrix(docs)
+m = as.matrix(dtm)
+v = sort(rowSums(m), decreasing = TRUE)
+d = data.frame(word = names(v), freq = v)
+head(d, 10)
+
+# Step 5: Generate the Word Cloud
+wordcloud(words = d$word, 
+          freq = d$freq,
+          min.freq = 1, 
+          max.words = 200,
+          random.order = FALSE, 
+          rot.per = 0.35, 
+          colors = brewer.pal(8, "Dark2"))
