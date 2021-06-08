e9b866afd505f39c0da5fc7e43f60d10179c09a9
YoViajo <rcrmj@hotmail.com>
2021-06-08 01:46:27 -0400

    Explorac datos

---
+++ csv_cortes_naturales.R
@@ -0,0 +1,30 @@
+# Clear plots
+if(!is.null(dev.list())) dev.off()
+# Clear console
+cat("\014") 
+# Clean workspace
+rm(list=ls())
+
+
+library(ggplot2)
+library(magrittr)
+library(dplyr)
+library(classInt)
+
+# Definir directorio de trabajo donde están los archivos CSV
+setwd("/home/yoviajo/Documentos/git_github/tesis/datos")
+
+
+# Leer el archivo CSV
+datos <- read.csv('tab_pais_numviajeros.csv')
+
+v <- as.numeric(as.character(datos$tot_viajeros))
+
+
+# Clasificación cortes naturales
+source("/home/yoviajo/Documentos/git_github/tesis/codigo_ref/plotjenks.r")
+res <- plotJenks(v, n=4)
+# clases creadas y número de observaciones que caen en cada clase
+res$classif
+# puntos de corte de las clases
+res$classif$brks
--- res_exploracion_datos.R
+++ res_exploracion_datos.R
@@ -10,9 +10,14 @@ rm(list=ls())
 library(ggplot2)
 library(magrittr)
 library(dplyr)
+library(classInt)
 
 # Definir directorio de trabajo donde están los archivos CSV
-setwd("/home/yoviajo/Documentos/lab/tesis/p11/extraido/opiniones/s01/790r")
+#setwd("/home/yoviajo/Documentos/lab/tesis/p11/extraido/opiniones/s01/790r")
+#setwd("/home/yoviajo/Documentos/lab/tesis/p11/extraido/opiniones/s03/580r")
+#setwd("/home/yoviajo/Documentos/lab/tesis/p11/extraido/opiniones/s04/250r")
+setwd("/home/yoviajo/Documentos/lab/tesis/p11/extraido/opiniones/s05/170r")
+
 
 # Leer el archivo CSV
 #datos <- read.csv('datos.csv')
@@ -90,8 +95,61 @@ v1 <- as.numeric(as.character(datos$a_num_aportes))
 # Quitar valores nulos del vector
 v2 <- v1[!is.na(v1)]
 # Cuál es el valor máximo?  = 4007 aportes!
+min(v2)
 max(v2)
+mean(v2)
+sd(v2)
 
 # Calcular y representar función de densidad
 densidad_aportes <- density(v2)
 plot(densidad_aportes)
+
+# Histograma
+hist(v2)
+
+# Agrupación en cuartiles
+df_aportes <- data.frame(v2)
+nro_compartim <- 4
+df_aportes<-df_aportes%>%mutate(MyQuantileBins = cut(v2, 
+                                     breaks = unique(quantile(v2,probs=seq.int(0,1, by=1/nro_compartim))), 
+                                     include.lowest=TRUE))
+head(df_aportes,10)
+
+df_aportes%>%group_by(MyQuantileBins)%>%count()
+
+# Clasificación cortes naturales
+source("/home/yoviajo/Documentos/git_github/tesis/codigo_ref/plotjenks.r")
+res <- plotJenks(v2, n=4)
+# clases creadas y número de observaciones que caen en cada clase
+res$classif
+# puntos de corte de las clases
+res$classif$brks
+
+
+## TEMPORALIDAD DE EXPERIENCIAS
+v_anios <- as.numeric(as.character(datos$exp_anio))
+v2_anios <- v_anios[!is.na(v_anios)]
+min(v2_anios)
+max(v2_anios)
+hist(v2_anios, nclass=11)
+
+t_viaje <- data.frame(datos$exp_anio,datos$exp_mes)
+colnames(t_viaje) <- c("anio","mes")
+
+# Gráfico de distribución de experiencias (viajes)
+mi_graf <- ggplot(t_viaje, aes(x = anio, y = mes)) +
+  geom_point()
+mi_graf +
+  scale_x_continuous(breaks = seq(2010, 2020, by = 1)) +
+  scale_y_continuous(breaks = seq(1, 12, by = 1)) +
+  labs(
+    x = "Año de viaje",
+    y = "Mes de viaje",
+    title = "Temporalidad de viajes: mes y año de visita",
+    #subtitle = "S01 - Güembé"
+    #subtitle = "S03 - Plaza 24 de Septiembre"
+    #subtitle = "S04 - Samaipata"
+    subtitle = "Iglesias de la Chiquitania"
+  )
+
+
